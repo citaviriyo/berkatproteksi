7ea8ae6c8ba3302cf8ed9e293098f5be90158137
citaviriyo <citaviriyo@gmail.com>
2026-01-01 01:44:52 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -544,7 +544,7 @@ export default function BerkatProteksi() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2025 Berkat Proteksi. All rights reserved.</p>
+            <p>&copy; 2026 Berkat Proteksi. All rights reserved.</p>
           </div>
         </div>
       </footer>
